21310cba527fbc79a34c4d45e9363b86247ac328
adhi1911 <amk2754@gmail.com>
2023-12-16 15:14:29 +0530

    Optimal Approach

--- Array/Second_largest_smallest.cpp
+++ Array/Second_largest_smallest.cpp
@@ -60,3 +60,41 @@ vector<int> getSecondOrderElements(int n, vector<int> a) {
     result.push_back(sec_smallest);
     return result;
 }
+
+
+//Optimal approach
+int secondlargest(vector<int> &a , int n){
+  int largest = a[0];
+  int s_largest = -1;
+  for(int i =1;i<n;i++){
+    if (a[i]>largest){
+      s_largest = largest;
+      largest = a[i];
+    } else if (a[i] < largest && a[i] > s_largest) {
+      s_largest = a[i];
+    }
+  }
+  return s_largest;
+}
+int secondsmallest(vector<int> &a, int n){
+  int smallest = a[0];
+  int s_smallest = INT_MAX;
+  for(int i =1;i<n;i++){
+    if(a[i]<smallest){
+      s_smallest = smallest;
+      smallest = a[i];
+    }
+    else if (a[i]>smallest && a[i] < s_smallest){
+      s_smallest = a[i];
+    }
+  }
+  return s_smallest;
+}
+
+
+vector<int> getSecondOrderElements(int n, vector<int> a) {
+    int sec_largest = secondlargest(a,n);
+    int sec_smallest = secondsmallest(a,n);
+
+    return {sec_largest , sec_smallest};
+}
