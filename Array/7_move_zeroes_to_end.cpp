fbf9cca88e2b9f48ec674920aa77add1960c7848
adhi1911 <amk2754@gmail.com>
2023-12-16 18:26:31 +0530

    Move zeroes to end

---
+++ Array/7_move_zeroes_to_end.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/move-zeroes/description/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int i =0;
+        for(int j =0;j<n;j++){
+            if (nums[j] != 0){
+                nums[i] = nums [j];
+                i++;
+            }           
+        }
+        for(int j = i; j<n;j++){
+            nums[j]=0;
+        }
+
+    }
+};
