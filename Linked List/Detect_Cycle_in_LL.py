4b48f6b0b1e3425b1b582097db5b1196e8fcbb6a
adhi1911 <amk2754@gmail.com>
2025-12-08 23:17:07 +0530

    Detect Cycle in LL : Brute Force

---
+++ Linked List/Detect_Cycle_in_LL.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+## Brute Force Approach
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        visited = set()
+        current = head 
+        while current:
+            if current in visited:
+                return True 
+            visited.add(current)
+            current = current.next
+
+        return False
