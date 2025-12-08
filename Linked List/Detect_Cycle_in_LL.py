c5f4bdee71c762d58a3dcfa1a928d5bee6727685
adhi1911 <amk2754@gmail.com>
2025-12-08 23:22:34 +0530

    Detect Cycle in LL : Floyd Algo

--- Linked List/Detect_Cycle_in_LL.py
+++ Linked List/Detect_Cycle_in_LL.py
@@ -6,14 +6,40 @@
 
 
 ## Brute Force Approach
+# class Solution:
+#     def hasCycle(self, head: Optional[ListNode]) -> bool:
+#         visited = set()
+#         current = head 
+#         while current:
+#             if current in visited:
+#                 return True 
+#             visited.add(current)
+#             current = current.next
+
+#         return False
+
+# FLoyd's Cycle-Finding Algorithm 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        visited = set()
-        current = head 
-        while current:
-            if current in visited:
+        if not head or not head.next:
+            return False 
+
+        slow = head
+        fast = head 
+        while fast and fast.next:
+            slow = slow.next 
+            fast = fast.next.next 
+
+            if slow == fast :
                 return True 
-            visited.add(current)
-            current = current.next
 
         return False
+
+# TC: O(N)
+# SC: O(1)
+## why this works: 
+# fast moves two steps ahead and slow only one
+# if there is a cycle, fast will be cycled back to the earlier nodes thus behind slow
+# so with each iteration the distance between them decreases by 1
+# eventually they will meet
+# if there is no cycle, fast will reach the end of the list (None) and iteration stops
